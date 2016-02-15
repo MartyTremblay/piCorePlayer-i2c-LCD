201c8968611a3aab6e6bc0cb615c8a79569bcd52
Martin Tremblay <marty@thetremblays.ca>
2016-02-15 09:25:19 -0500

    Adding PyLMS submodule

--- piCorePlayer-i2c-LCD.py
+++ piCorePlayer-i2c-LCD.py
@@ -19,12 +19,42 @@ global sq
 global clear_line
 global lcd_char_width
 
+fontdata = [
+    # Char 0  - 0%
+    [0x00,0x01,0x00,0x00,0x01,0x00,0x00,0x01],
+    # Char 1 - 14%
+    [0x00,0x01,0x00,0x00,0x01,0x00,0x00,0x03],
+    # Char 2 - 28%
+    [0x00,0x01,0x00,0x00,0x01,0x00,0x02,0x03],
+    # Char 3 - 43%
+    [0x00,0x01,0x00,0x00,0x01,0x02,0x02,0x03],
+    # Char 4 - 57%
+    [0x00,0x01,0x00,0x00,0x03,0x02,0x02,0x03],
+    # Char 5 - 71%
+    [0x00,0x01,0x00,0x02,0x03,0x02,0x02,0x03],
+    # Char 6 - 85%
+    [0x00,0x01,0x02,0x02,0x03,0x02,0x02,0x03],
+    # Char 7 - 100%
+    [0x00,0x05,0x04,0x04,0x05,0x04,0x04,0x05],
+
+    # Char 8 - play
+    [0x00,0x00,0x00,0x08,0x0c,0x0e,0x0c,0x08],
+    # Char 9 - paused
+    [0x00,0x00,0x00,0x1b,0x1b,0x1b,0x1b,0x1b],
+    # Char 10 - stop
+    [0x00,0x00,0x00,0x1f,0x1f,0x1f,0x1f,0x1f],
+    # Char 11 - progress
+    [0x00,0x00,0x00,0x00,0x00,0x15,0x00,0x00],
+]
+
 lcd_char_width = 16
 clear_line = " " * lcd_char_width
 lcd_first_line = ""
 lcd_second_line = ""
 
 mylcd = RPi_I2C_driver.lcd()
+mylcd.lcd_load_custom_chars(fontdata)
+
 #ip = "192.168.101.12"
 ip = ""
 print "Using squeezelite to obtain ip address of server..."	
@@ -44,27 +74,30 @@ print "Version: %s" % sc.get_version()
 mac = ':'.join(['{:02x}'.format((get_mac()  >> i) & 0xff) for i in range(0,8*6,8)][::-1])
 print  mac                                        
 sq = sc.get_player(mac)
-mylcd.lcd_display_string("".join(sq.get_ip_address()).center(lcd_char_width), 1)
-mylcd.lcd_display_string("".join(sq.get_name()).center(lcd_char_width), 2)
+#mylcd.lcd_display_string("".join(sq.get_ip_address()).center(lcd_char_width), 1)
+#mylcd.lcd_display_string("".join(sq.get_name()).center(lcd_char_width), 2)
 
-sleep(3) 
+#sleep(3) 
 
 def print_progress_bar() :
     global mylcd, sq, lcd_char_width, clear_line, lcd_second_line
-    lcd_text = ""
+    #lcd_text = ""
     if sq.get_mode() == "pause" :
-        lcd_text = "Paused".center(lcd_char_width)
+        mylcd.lcd_display_string_pos(unichr(9),2,0)
     elif sq.get_mode() == "stop" :
-        lcd_text = "Stopped".center(lcd_char_width)
+        mylcd.lcd_display_string_pos(unichr(10),2,0)
     elif sq.get_track_duration() > 0 : 
-        percentage = float(sq.get_time_elapsed()) / float(sq.get_track_duration())
-        lcd_text = "." * int(percentage * lcd_char_width) + "                "
+        percentage_played = float(sq.get_time_elapsed()) / float(sq.get_track_duration())
+        lcd_text = unichr(11) * int(percentage_played * (lcd_char_width - 1)) + " "
+        #if lcd_text != lcd_second_line :
+        lcd_second_line = lcd_text
+        mylcd.lcd_display_string_pos(unichr(8) + lcd_text, 2, 0)
+        mylcd.lcd_display_string_pos(unichr(int(sq.get_volume() * .07)),2,15)
+        #print int(sq.get_volume() * .05)
     else :
         lcd_text = "Streaming".center(lcd_char_width)
     
-    if lcd_text != lcd_second_line :
-        lcd_second_line = lcd_text
-        mylcd.lcd_display_string(lcd_text, 2)
+    
 
 
 def print_time_remaining() :
@@ -83,7 +116,7 @@ def print_time_remaining() :
         if minutes < 10 :
             minutes = " ".join(minutes)
         lcd_text = "     %s : %s     " % ( minutes, seconds )
-    mylcd.lcd_display_string(lcd_text, 2)
+    mylcd.lcd_display_string(lcd_text, 2) 
     
     
 def run() :
